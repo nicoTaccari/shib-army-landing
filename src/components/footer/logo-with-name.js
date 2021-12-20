23430359b3363cbef4a13b3d04181b52388e7cdc
Nicolas <nicolastaccari@gmail.com>
2021-12-20 17:05:38 +0100

    footer + terms and conditions

---
+++ src/components/footer/logo-with-name.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Logo from "../../assets/images/icons/Logo.png";
+
+export default function LogoWithName({ description }) {
+  return (
+    <div className="logo-column">
+      <div className="logo-row">
+        <div className="logo">
+          <img src={Logo} alt="shiba logo" />
+        </div>
+        <h2 className="footer-name">Shiba Army</h2>
+      </div>
+      {description && (
+        <p className="description">
+          8,216 unique shibas ready to make profits.
+        </p>
+      )}
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Terms from "./components/terms";
+import ScrollToTop from "./components/wrappers/scroll-to-top";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="terms" element={<Terms />} />
+        </Routes>
+      </ScrollToTop>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
