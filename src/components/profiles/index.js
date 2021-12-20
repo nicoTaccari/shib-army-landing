dd7b1bd0cc75358f109d2ed9b4cbaa024e3cb38c
Nicolas <nicolastaccari@gmail.com>
2021-12-20 04:03:28 +0100

    team section

--- src/components/profiles/index.js
+++ src/components/profiles/index.js
@@ -6,12 +6,50 @@ import Mong from "../../assets/images/Mong.jpg";
 
 import Profile from "./profile";
 
+const team = {
+  hawk: {
+    position: "Co-Founder & technical development",
+    content:
+      "+5 years experience in cyber security, blockchain and digital forensics working as a team lead for a big company with 50k+ employees.",
+  },
+  lion: {
+    position: "Co-Founder & Head of Marketing",
+    content:
+      "+5 years experience in Marketing (media analyst, digital marketing and social media marketing) and working for one of the biggest tv-media companies. Worked in the past with big brands like NFL, Disney, Gold´s Gym and WWE!",
+  },
+  mong: {
+    position: "Art Director & Designer",
+    content:
+      "Official degree in graphic design (University of China) with +3 years of professional experience in digital drawing. Has drawn as a pseudonym in the past many children's books for major brands.",
+  },
+};
+
 function Profiles(props) {
   return (
     <div className="profile row">
-      <Profile border={true} name={"Hawkenase"} image={Hawk} />
-      <Profile border={true} name={"Lion King"} image={Lion} />
-      <Profile border={false} name={"Mong"} image={Mong} />
+      <Profile
+        border={true}
+        name={"Hawkenase"}
+        image={Hawk}
+        position={team.hawk.position}
+        content={team.hawk.content}
+      />
+      <Profile
+        border={true}
+        name={"Lion King"}
+        image={Lion}
+        position={team.lion.position}
+        content={team.lion.content}
+      />
+      <Profile
+        border={true}
+        name={"Mong"}
+        image={Mong}
+        position={team.mong.position}
+        content={team.mong.content}
+      />
+      {/* <Profile border={true} name={"Lion King"} image={Lion} />
+      <Profile border={false} name={"Mong"} image={Mong} /> */}
     </div>
   );
 }
--- src/components/profiles/profile.js
+++ src/components/profiles/profile.js
@@ -2,25 +2,29 @@ import React from "react";
 import { FaTwitter } from "react-icons/fa";
 import { SiInstagram } from "react-icons/si";
 
-export default function Profile({ border, name, image }) {
+export default function Profile({ name, image, position, content }) {
   return (
-    <div
-      className={
-        border ? "profile-container border-right" : "profile-container"
-      }
-    >
-      <div className="profile-info">
-        <h2>{name}</h2>
-        <div className="icons-container">
-          <a href="#">
-            <FaTwitter />
-          </a>
-          <a href="#">
-            <SiInstagram />
-          </a>
+    <div className="profile-container">
+      <div className="profile-image-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="profile-info-column">
+        <div className="profile-info">
+          <h2>{name}</h2>
+          <div className="icons-container">
+            <a href="#">
+              <FaTwitter />
+            </a>
+            <a href="#">
+              <SiInstagram />
+            </a>
+          </div>
+        </div>
+        <div className="profile-content">
+          <h3>{position}</h3>
+          <p>{content}</p>
         </div>
       </div>
-      <img src={image} alt="hawk" />
     </div>
   );
 }
