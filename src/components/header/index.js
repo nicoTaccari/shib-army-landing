47856e8917e2ddea9fab8719e47a3ca8d8744501
Nicolas <nicolastaccari@gmail.com>
2021-12-21 18:24:17 +0100

    header video con scroll

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./styles.css";
 import HeaderVideo from "../../assets/videos/Banner.mp4";
+import { VideoScroll } from "react-video-scroll";
 
 export default function Header() {
   return (
     <section className="header-section">
-      <video className="header-video" autoPlay loop muted playsInline poster="">
-        <source src={HeaderVideo} type="video/mp4" />
-      </video>
+      <VideoScroll playbackRate={130}>
+        <video
+          className="header-video"
+          tabIndex="0"
+          autobuffer="autobuffer"
+          preload="preload"
+          muted
+          playsInline
+        >
+          <source src={HeaderVideo} type="video/mp4" />
+        </video>
+      </VideoScroll>
     </section>
   );
 }
