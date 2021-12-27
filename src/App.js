362486849f11e862bdbc5f72131eb8c863fda7c5
Nicolas <nicolastaccari@gmail.com>
2021-12-27 18:34:45 +0100

    added react lazy loading, suspense and spinner

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Suspense } from "react";
 
 import "./App.css";
-import Roadmap from "./components/roadmap/index";
-import Header from "./components/header/index";
-import Gallery from "./components/gallery";
-import About from "./components/about";
-import Footer from "./components/footer";
-import Team from "./components/team";
-import Profiles from "./components/profiles";
-import HeaderRow from "./components/header-row";
+import Spinner from "./components/spinner";
+
+const HeaderRow = React.lazy(() => import("./components/header-row"));
+const Roadmap = React.lazy(() => import("./components/roadmap"));
+const Header = React.lazy(() => import("./components/header"));
+const Gallery = React.lazy(() => import("./components/gallery"));
+const About = React.lazy(() => import("./components/about"));
+const Footer = React.lazy(() => import("./components/footer"));
+const Team = React.lazy(() => import("./components/team"));
+const Profiles = React.lazy(() => import("./components/profiles"));
 
 const App = () => {
   return (
-    <Suspense fallback={"Loading..."}>
+    <Suspense fallback={<Spinner />}>
       <HeaderRow />
       <Header />
       <About />
---
+++ src/components/spinner/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function Spinner() {
+  return (
+    <div class="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
