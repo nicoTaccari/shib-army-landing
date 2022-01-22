f552b5235fedcc4f6cf5cf8abfba2acf7d3ef0a4
Nicolas <nicolastaccari@gmail.com>
2022-01-22 12:09:26 +0100

    fixed discord links

--- src/components/terms/index.js
+++ src/components/terms/index.js
@@ -17,7 +17,7 @@ export default function Terms() {
             <a
               rel="noreferrer"
               target="_blank"
-              href="https://discord.gg/K88Rn2D9y8"
+              href="https://discord.gg/shibarmynft"
             >
               <SiDiscord />
             </a>
