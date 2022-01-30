7153c53537f64d38c3070e1e13cd499a9d046bd5
Nicolas <nicolastaccari@gmail.com>
2022-01-30 19:24:17 +0100

    added parallax

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import HeaderVideo from "../../assets/videos/Banner.mp4";
-import { VideoScroll } from "react-video-scroll";
+// import { VideoScroll } from "react-video-scroll";
 
 export default function Header() {
-  return (
-    <section className="header-section">
-      <VideoScroll playbackRate={130}>
-        <video
-          className="header-video"
-          tabIndex="0"
-          autobuffer="autobuffer"
-          preload="preload"
-          muted
-          playsInline
-        >
-          <source src={HeaderVideo} type="video/mp4" />
-        </video>
-      </VideoScroll>
-    </section>
+
+  const handleScroll = () => {
+    setDivStyle({ transform: `translateY(${window.scrollY}px)` })
+  }
+
+  const [divStyle, setDivStyle] = useState({ transform: 'translateY(0px)' })
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+
+    };
+  }, []);
+
+
+  return (<section className="header-section">
+    <div style={divStyle}>
+
+      {/* <VideoScroll> */}
+      <video
+        className="header-video"
+        tabIndex="0"
+        preload="preload"
+        muted
+        playsInline
+      >
+        <source src={HeaderVideo} type="video/mp4" />
+      </video>
+      {/* </VideoScroll> */}
+    </div>
+  </section>
   );
 }
